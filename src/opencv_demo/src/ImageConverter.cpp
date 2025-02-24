5b8dc8bcf6d53e78483f895b48a36df1f17866ba
AdamLevin7 <adamarilevin@gmail.com>
2025-02-24 23:31:21 +0000

    IT WORKS OMG IT WORKS OMG

--- src/opencv_demo/src/ImageConverter.cpp
+++ src/opencv_demo/src/ImageConverter.cpp
@@ -1,62 +1,80 @@
-/*#include <stdlib.h>
-#include <functional>
+#include "ImageConverter.h"
 
-#include "rclcpp/rclcpp.hpp"
-#include "image_transport/image_transport.hpp"
-#include <cv_bridge/cv_bridge.h>
-#include "sensor_msgs/image_encodings.hpp"
+//much thanks: https://gist.github.com/nightduck/a07c185faad82aeaacbfa87298d035c0
+ImageConverter::ImageConverter() : Node("ImageConverter"){
+    // this window is just to show openCV changes
+    cv::namedWindow("Image window");
+    rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
+    pub_ = this->create_publisher<std_msgs::msg::Float32>("desired_angle", 10);
+    curr_angle_sub_ = this->create_subscription<std_msgs::msg::Float32>(
+            "current_angle", 10, std::bind(&ImageConverter::angleCallback, this, _1));
+    sub_ = image_transport::create_subscription(
+            this, "robotcam",std::bind(&ImageConverter::imageCallback,
+                                       this, _1), "raw", custom_qos);
+}
 
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//callback to subscribe to current angle and put value in curr_angle
+void ImageConverter::angleCallback(const std_msgs::msg::Float32::SharedPtr msg){
+    curr_angle = msg->data;
+}
 
-class ImageConverter : public rclcpp::Node {
-private:
-    const std::string OPENCV_WINDOW = "Image window";
-//    image_transport::ImageTransport it;
-    image_transport::Subscriber sub_;
-    image_transport::Publisher pub_;
+// image callback which receives and processes image and then publishes angle
+void ImageConverter::imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
+    cv_bridge::CvImagePtr cv_ptr;
+    //use cv_bridge to convert ROS image to OpenCV image
+    cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+    // make sure image is large enough
+    float x = 0.0f;
+    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+        // Draw circle
+        cv::circle(cv_ptr->image, cv::Point(150, 150), 10, CV_RGB(0,255,0));
+    cv::Mat hsv_image;
+    cv::cvtColor(cv_ptr->image, hsv_image, cv::COLOR_BGR2HSV);
+    cv::Scalar lower_red1(0, 120, 70); // Lower bound for red
+    cv::Scalar upper_red1(10, 255, 255); // Upper bound for red (lower hue)
+    cv::Scalar lower_red2(170, 120, 70); // Lower bound for red (upper hue)
+    cv::Scalar upper_red2(180, 255, 255); 
+    cv::Mat red_hue_image;
+    cv::Mat mask1, mask2;
+    cv::inRange(hsv_image, lower_red1, upper_red1, mask1); // Mask for lower red range
+    cv::inRange(hsv_image, lower_red2, upper_red2, mask2); // Mask for upper red range
 
-    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg) {
-        cv_bridge::CvImagePtr cv_ptr;
-        try
+    // Combine the two masks
+    cv::Mat red_mask;
+    cv::bitwise_or(mask1, mask2, red_mask);
+    std::vector<std::vector<cv::Point>> contours;
+    cv::findContours(red_mask, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    if (!contours.empty())
+      {
+        int max_area = 0;
+        cv::Point center;
+        for (const auto &contour : contours)
         {
-            cv_ptr = cv_bridge::toCvCopy(msg, msg->encoding);
+          int area = cv::contourArea(contour);
+          if (area > max_area)
+          {
+            max_area = area;
+            cv::Moments m = cv::moments(contour);
+            center = cv::Point(m.m10 / m.m00, m.m01 / m.m00);
+          }
         }
-        catch (cv_bridge::Exception& e)
-        {
-            RCLCPP_ERROR(this->get_logger(), "cv_bridge exception: %s", e.what());
-            return;
-        }
-
-        if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-            cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
-
-        cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-        cv::waitKey(3);
-        pub_.publish(cv_ptr->toImageMsg());
-    }
-
-public:
-    ImageConverter() : Node("image_converter") {
-//    ImageConverter() : Node("image_converter"), it(this->get()) {
-
-        // Open demo window that will show output image
-        cv::namedWindow(OPENCV_WINDOW);
-
-        rmw_qos_profile_t custom_qos = rmw_qos_profile_default;
-        pub_ = image_transport::create_publisher(this, "out_image_base_topic", custom_qos);
-        sub_ = image_transport::create_subscription(this, "in_image_base_topic",
-                std::bind(&ImageConverter::imageCallback, this, std::placeholders::_1), "raw", custom_qos);
-
-//        pub = it.advertise("out_image_base_topic", 1);
-//        sub = it.subscribe("in_image_base_topic", 1, imageCallback);
-    }
-
-    ~ImageConverter()
-    {
-        cv::destroyWindow(OPENCV_WINDOW);
-    }
-};
+        x = center.x;
+      } 
+    // Display image with circle
+    cv::imshow("Image window", cv_ptr->image);
+    cv::waitKey(3);
+    
+    // Create a message object for publishing with a fixed value
+    float PI = 3.14159265f; //idk how to get actual pi
+    float resX = 640.0f; //resolution i think
+    float FOV = 90.0f; //might be 100? not really sure
+    float angle = curr_angle+FOV/resX*(320-x)*PI/180.0;
+    auto output_msg = std_msgs::msg::Float32();
+    output_msg.data = angle;
+    RCLCPP_INFO(this->get_logger(), "its the final value %f", x);
+    pub_->publish(output_msg);
+}
 
 int main(int argc, char** argv)
 {
@@ -64,5 +82,4 @@ int main(int argc, char** argv)
     rclcpp::spin(std::make_shared<ImageConverter>());
     rclcpp::shutdown();
     return 0;
-} 
-*/
+}
---
+++ src/opencv_demo/src/ImageConverter.h
@@ -0,0 +1,34 @@
+//
+// Created by bvox0102 on 11/28/23.
+//
+
+#ifndef Adam_Levin_Training_2_Image_Converter
+#define Adam_Levin_Training_2_Image_Converter
+
+#include <stdlib.h>
+#include <functional>
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "std_msgs/msg/header.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include <cv_bridge/cv_bridge.h> // cv_bridge converts between ROS 2 image messages and OpenCV image representations.
+#include <image_transport/image_transport.hpp> // publish and subscribe to compressed image streams in ROS2
+#include <opencv2/opencv.hpp> // include everything about OpenCV
+
+using std::placeholders::_1;
+
+class ImageConverter : public rclcpp::Node {
+public:
+    ImageConverter();
+private:
+    void imageCallback(const sensor_msgs::msg::Image::ConstSharedPtr &msg);
+    void angleCallback(const std_msgs::msg::Float32::SharedPtr msg);
+    rclcpp::Publisher<std_msgs::msg::Float32>::SharedPtr pub_;
+    rclcpp::Subscription<std_msgs::msg::Float32>::SharedPtr curr_angle_sub_;
+
+    float curr_angle;
+    image_transport::Subscriber sub_;
+};
+
+#endif //Adam_Levin_Training_2_Image_Converter
